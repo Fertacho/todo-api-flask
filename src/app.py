502c698601fc136d87ce1686ffb9f8252bd0a547
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-07-12 01:36:28 +0000

    avance

--- src/app.py
+++ src/app.py
@@ -1,2 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+
 app = Flask(__name__)
+
+todos = [ { "label": "Sample", "done": True }]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    json_text = jsonify(request_body)
+    print("Incoming request with the following body", request_body)
+    return json_text
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
